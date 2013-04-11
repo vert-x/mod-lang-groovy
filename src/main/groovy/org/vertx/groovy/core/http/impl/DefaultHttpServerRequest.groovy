63e58ab8f56e0978917becb4ef6f68e01f75c914
purplefox <timvolpe@gmail.com>
2013-04-11 15:01:57 +0100

    api_refactor

---
+++ src/main/groovy/org/vertx/groovy/core/http/impl/DefaultHttpServerRequest.groovy
@@ -0,0 +1,124 @@
+package org.vertx.groovy.core.http.impl
+
+import org.vertx.groovy.core.buffer.Buffer
+import org.vertx.groovy.core.http.HttpServerRequest
+import org.vertx.groovy.core.http.HttpServerResponse
+import org.vertx.java.core.Handler
+
+import javax.net.ssl.SSLPeerUnverifiedException
+import javax.security.cert.X509Certificate
+
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+class DefaultHttpServerRequest implements HttpServerRequest {
+
+  private org.vertx.java.core.http.HttpServerRequest jRequest
+
+  private HttpServerResponse response
+
+  DefaultHttpServerRequest(org.vertx.java.core.http.HttpServerRequest jRequest) {
+    this.jRequest = jRequest
+    this.response = new DefaultHttpServerResponse(jRequest.response())
+  }
+
+  @Override
+  String getMethod() {
+    jRequest.method()
+  }
+
+  @Override
+  String getUri() {
+    jRequest.uri()
+  }
+
+  @Override
+  String getPath() {
+    jRequest.path()
+  }
+
+  @Override
+  String getQuery() {
+    jRequest.query()
+  }
+
+  @Override
+  HttpServerResponse getResponse() {
+    response
+  }
+
+  @Override
+  Map<String, String> getHeaders() {
+    jRequest.headers()
+  }
+
+  @Override
+  Map<String, String> getParams() {
+    jRequest.params()
+  }
+
+  @Override
+  InetSocketAddress getRemoteAddress() {
+    jRequest.remoteAddress()
+  }
+
+  @Override
+  X509Certificate[] getPeerCertificateChain() throws SSLPeerUnverifiedException {
+    jRequest.peerCertificateChain()
+  }
+
+  @Override
+  URI getAbsoluteURI() {
+    jRequest.absoluteURI()
+  }
+
+  @Override
+  void bodyHandler(Closure handler) {
+    jRequest.bodyHandler(({handler(new Buffer(it))} as Handler))
+  }
+
+  @Override
+  HttpServerRequest dataHandler(Closure handler) {
+    jRequest.dataHandler(({handler(new Buffer(it))} as Handler))
+    this
+  }
+
+  @Override
+  HttpServerRequest pause() {
+    jRequest.pause()
+    this
+  }
+
+  @Override
+  HttpServerRequest resume() {
+    jRequest.resume()
+    this
+  }
+
+  @Override
+  HttpServerRequest endHandler(Closure endHandler) {
+    jRequest.endHandler(endHandler as Handler)
+    this
+  }
+
+  @Override
+  HttpServerRequest exceptionHandler(Closure handler) {
+    jRequest.exceptionHandler(handler as Handler)
+    this
+  }
+
+}
---
+++ src/main/groovy/org/vertx/groovy/core/http/impl/DefaultHttpServerResponse.groovy
@@ -0,0 +1,192 @@
+package org.vertx.groovy.core.http.impl
+
+import org.vertx.groovy.core.buffer.Buffer
+import org.vertx.groovy.core.http.HttpServerResponse
+import org.vertx.java.core.AsyncResultHandler
+import org.vertx.java.core.Handler
+
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+class DefaultHttpServerResponse implements HttpServerResponse {
+
+  private org.vertx.java.core.http.HttpServerResponse jResponse
+
+  DefaultHttpServerResponse(org.vertx.java.core.http.HttpServerResponse jResponse) {
+    this.jResponse = jResponse
+  }
+
+  @Override
+  int getStatusCode() {
+    jResponse.statusCode
+  }
+
+  @Override
+  HttpServerResponse setStatusCode(int statusCode) {
+    jResponse.setStatusCode(statusCode)
+    this
+  }
+
+  @Override
+  String getStatusMessage() {
+    jResponse.getStatusMessage()
+  }
+
+  @Override
+  HttpServerResponse setStatusMessage(String statusMessage) {
+    jResponse.setStatusMessage(statusMessage)
+    this
+  }
+
+  @Override
+  HttpServerResponse setChunked(boolean chunked) {
+    jResponse.setChunked(chunked)
+    this
+  }
+
+  @Override
+  boolean isChunked() {
+    jResponse.isChunked()
+  }
+
+  @Override
+  Map<String, Object> getHeaders() {
+    jResponse.headers()
+  }
+
+  @Override
+  HttpServerResponse putHeader(String name, Object value) {
+    jResponse.putHeader(name, value)
+    this
+  }
+
+  @Override
+  Map<String, Object> getTrailers() {
+    jResponse.trailers()
+  }
+
+  @Override
+  HttpServerResponse putTrailer(String name, Object value) {
+    jResponse.putTrailer(name, value)
+    this
+  }
+
+  @Override
+  void closeHandler(Closure handler) {
+    jResponse.closeHandler(handler as Handler)
+  }
+
+  @Override
+  HttpServerResponse write(Buffer chunk) {
+    jResponse.write(chunk.toJavaBuffer())
+    this
+  }
+
+  @Override
+  HttpServerResponse setWriteQueueMaxSize(int maxSize) {
+    jResponse.setWriteQueueMaxSize(maxSize)
+    this
+  }
+
+  @Override
+  boolean isWriteQueueFull() {
+    jResponse.writeQueueFull()
+  }
+
+  @Override
+  HttpServerResponse drainHandler(Closure handler) {
+    jResponse.drainHandler(handler as Handler)
+    this
+  }
+
+  @Override
+  HttpServerResponse write(String chunk, String enc) {
+    jResponse.write(chunk, enc)
+    this
+  }
+
+  @Override
+  HttpServerResponse write(String chunk) {
+    jResponse.write(chunk)
+    this
+  }
+
+  @Override
+  HttpServerResponse write(Buffer chunk, Closure doneHandler) {
+    jResponse.write(chunk.toJavaBuffer(), doneHandler as AsyncResultHandler)
+    this
+  }
+
+  @Override
+  HttpServerResponse write(String chunk, String enc, Closure doneHandler) {
+    jResponse.write(chunk, enc, doneHandler as AsyncResultHandler)
+    this
+  }
+
+  @Override
+  HttpServerResponse write(String chunk, Closure doneHandler) {
+    jResponse.write(chunk, doneHandler as AsyncResultHandler)
+    this
+  }
+
+  @Override
+  void end(String chunk) {
+    jResponse.end(chunk)
+  }
+
+  @Override
+  void end(String chunk, String enc) {
+    jResponse.end(chunk, enc)
+  }
+
+  @Override
+  void end(Buffer chunk) {
+    jResponse.end(chunk.toJavaBuffer())
+  }
+
+  @Override
+  void end() {
+    jResponse.end()
+  }
+
+  @Override
+  HttpServerResponse sendFile(String filename) {
+    jResponse.sendFile(filename)
+    this
+  }
+
+  @Override
+  void close() {
+    jResponse.close()
+  }
+
+  @Override
+  HttpServerResponse leftShift(Buffer buff) {
+    return write(buff)
+  }
+
+  @Override
+  HttpServerResponse leftShift(String str) {
+    return write(str)
+  }
+
+  @Override
+  HttpServerResponse exceptionHandler(Closure handler) {
+    jResponse.exceptionHandler(handler as Handler)
+    this
+  }
+}
