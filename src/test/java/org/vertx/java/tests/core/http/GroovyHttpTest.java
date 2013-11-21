59461227cb8ce18e13a27916be200db2660e628f
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2013-11-21 11:45:43 +0100

    Adding HTTP Compression tests

--- src/test/java/org/vertx/java/tests/core/http/GroovyHttpTest.java
+++ src/test/java/org/vertx/java/tests/core/http/GroovyHttpTest.java
@@ -180,6 +180,151 @@ public class GroovyHttpTest extends TestBase {
     startTest(getMethodName());
   }
 
+
+  public void testGETCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testGETChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testGETSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testGETSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPUTCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPUTChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPUTSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPUTSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPOSTCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPOSTChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPOSTSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPOSTSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testHEADCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testHEADChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testHEADSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testHEADSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testOPTIONSCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testOPTIONSChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testOPTIONSSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testOPTIONSSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testDELETECompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testDELETEChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testDELETESSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testDELETESSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testTRACECompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testTRACEChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testTRACESSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testTRACESSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testCONNECTCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testCONNECTChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testCONNECTSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testCONNECTSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPATCHCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPATCHChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPATCHSSLCompressed() {
+    startTest(getMethodName());
+  }
+
+  public void testPATCHSSLChunkedCompressed() {
+    startTest(getMethodName());
+  }
+
   public void testFormFileUpload() {
     startTest(getMethodName());
   }
